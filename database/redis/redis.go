cd2ad3ae1011e0c06d83a1cfb96a220dc524d93f
DizzyZ7 <99655528+DizzyZ7@users.noreply.github.com>
2025-10-22 22:11:17 +0300

    Update redis.go
    Implemented Redis client connection

--- database/redis/redis.go
+++ database/redis/redis.go
@@ -3,25 +3,28 @@ package redis
 import (
     "context"
     "log"
+    "time"
 
     "github.com/redis/go-redis/v9"
 )
 
-// Client содержит клиент Redis.
-var Client *redis.Client
+// Client представляет клиент для работы с кэшем Redis.
+type Client struct {
+    *redis.Client
+}
 
-// InitRedis устанавливает соединение с Redis.
-func InitRedis(redisURL string) {
-    Client = redis.NewClient(&redis.Options{
+// NewClient устанавливает соединение с Redis и возвращает клиент.
+func NewClient(redisURL string) *Client {
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    rdb := redis.NewClient(&redis.Options{
         Addr: redisURL,
     })
 
-    // Проверяем соединение
-    ctx := context.Background()
-    _, err := Client.Ping(ctx).Result()
-    if err != nil {
+    if _, err := rdb.Ping(ctx).Result(); err != nil {
         log.Fatalf("Не удалось подключиться к Redis: %v", err)
     }
-
-    log.Println("Успешно подключились к Redis!")
+    log.Println("Успешное подключение к Redis!")
+    return &Client{rdb}
 }
