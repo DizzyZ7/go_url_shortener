343a0d746a73df445bc767e82deb0c57efc12fc4
DizzyZ7 <99655528+DizzyZ7@users.noreply.github.com>
2025-10-22 22:08:57 +0300

    Update redis.go
    Added Redis connection logic.

--- database/redis/redis.go
+++ database/redis/redis.go
@@ -1,3 +1,27 @@
 package redis
 
-// Код для работы с Redis
+import (
+    "context"
+    "log"
+
+    "github.com/redis/go-redis/v9"
+)
+
+// Client содержит клиент Redis.
+var Client *redis.Client
+
+// InitRedis устанавливает соединение с Redis.
+func InitRedis(redisURL string) {
+    Client = redis.NewClient(&redis.Options{
+        Addr: redisURL,
+    })
+
+    // Проверяем соединение
+    ctx := context.Background()
+    _, err := Client.Ping(ctx).Result()
+    if err != nil {
+        log.Fatalf("Не удалось подключиться к Redis: %v", err)
+    }
+
+    log.Println("Успешно подключились к Redis!")
+}
