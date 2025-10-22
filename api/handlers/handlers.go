4db184d87b26113328264fd177fcc867403ed2ce
DizzyZ7 <99655528+DizzyZ7@users.noreply.github.com>
2025-10-22 21:48:28 +0300

    Create handlers.go
    Created api/handlers directory and handlers.go

---
+++ api/handlers/handlers.go
@@ -0,0 +1,10 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+// Обработчики запросов будут здесь
+func ShortenURLHandler(c *gin.Context) {
+}
+
+func RedirectURLHandler(c *gin.Context) {
+}
