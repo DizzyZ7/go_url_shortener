798e8b49c602404a09c035a3919056b6cfb14eea
DizzyZ7 <99655528+DizzyZ7@users.noreply.github.com>
2025-10-22 22:17:38 +0300

    Update main.go
    Configured Gin to load HTML templates

--- main.go
+++ main.go
@@ -1,34 +1,36 @@
 package main
 
 import (
-	"log"
+    "log"
 
-	"github.com/gin-gonic/gin"
-	"go_url_shortener/api/routes"
-	"go_url_shortener/config"
-	"go_url_shortener/database/postgres"
-	"go_url_shortener/database/redis"
+    "github.com/gin-gonic/gin"
+    "go_url_shortener/api/routes"
+    "go_url_shortener/config"
+    "go_url_shortener/database/postgres"
+    "go_url_shortener/database/redis"
 )
 
 func main() {
-	// Загрузка конфигурации
-	cfg := config.LoadConfig()
+    // Загрузка конфигурации
+    cfg := config.LoadConfig()
 
-	// Подключение к базам данных
-	pgDB := postgres.NewDB(cfg.PostgresURL)
-	defer pgDB.Close()
-	pgDB.Migrate()
+    // Подключение к базам данных
+    pgDB := postgres.NewDB(cfg.PostgresURL)
+    defer pgDB.Close()
+    pgDB.Migrate()
 
-	redisClient := redis.NewClient(cfg.RedisURL)
-	defer redisClient.Close()
+    redisClient := redis.NewClient(cfg.RedisURL)
+    defer redisClient.Close()
 
-	// Настройка Gin-роутера
-	router := gin.Default()
-	routes.SetupRoutes(router, pgDB, redisClient)
+    // Настройка Gin-роутера
+    router := gin.Default()
+    router.LoadHTMLGlob("views/*") // Загрузка HTML-шаблонов
 
-	// Запуск сервера
-	log.Printf("Сервер запущен на %s", cfg.ServerAddress)
-	if err := router.Run(cfg.ServerAddress); err != nil {
-		log.Fatalf("Не удалось запустить сервер: %v", err)
-	}
+    routes.SetupRoutes(router, pgDB, redisClient)
+
+    // Запуск сервера
+    log.Printf("Сервер запущен на %s", cfg.ServerAddress)
+    if err := router.Run(cfg.ServerAddress); err != nil {
+        log.Fatalf("Не удалось запустить сервер: %v", err)
+    }
 }
